7ec21b10176ca17641c1e3b370cba2839963bd3b
Ezequiel Garcia <egarcia.informatic.ing@gmail.com>
2020-08-21 19:39:50 -0400

    added trait Template
    This trait expose methods for compile and prepare component metadata for the rendering.

--- src/Template/Template.php
+++ src/Template/Template.php
@@ -2,14 +2,149 @@
 
 namespace Core\Template;
 
+use Core\Template\Render\Data;
+use Core\Utils\Tools;
+
+use ReflectionClass;
+use ReflectionMethod;
+use ReflectionProperty;
+use Error;
+
 trait Template
 {
-    static public function render()
+
+    private static $template_file_conf = [
+        "ext" => "template.html",
+    ];
+
+    private static $__template_name = null;
+
+    private function __reflectPropertyes(ReflectionClass &$reflection)
     {
+        $data = [];
+        $propertyes = $reflection->getProperties(ReflectionProperty::IS_PUBLIC | ReflectionProperty::IS_PROTECTED);
+
+        $blacklist = [
+            "template_file_conf"
+        ];
+
+        foreach ($propertyes as $key => $prop) {
+            $name = $prop->getName();
+            if (false === array_search($name, $blacklist)) {
+                $data[$name] = $prop->getValue($this);
+            }
+        }
+
+        return $data;
     }
 
-    static public function getTemplateComponentDir()
+    private function __reflectMethods(ReflectionClass &$reflection)
     {
-        return __DIR__ . DIRECTORY_SEPARATOR . __FILE__ . "::class " . __CLASS__;
+        $data = [];
+        $methods = $reflection->getMethods(ReflectionMethod::IS_PUBLIC | ReflectionMethod::IS_PROTECTED);
+
+        $blacklist = [
+            "__reflectPropertyes",
+            "__reflectMethods",
+            "__reflectDataFromSure",
+            "__toRender",
+            "__toString",
+            "__invoke",
+            "__construct"
+        ];
+
+        foreach ($methods as $key => $method) {
+            $name = $method->getName();
+            if (false === array_search($name, $blacklist)) {
+                $data[$name] = $method->getClosure($this);
+            }
+        }
+
+        return $data;
+    }
+
+    private static function __reflectDataFromSure(ReflectionClass &$reflection)
+    {
+        $data = [];
+        $methods = $reflection->getMethods(ReflectionMethod::IS_PUBLIC | ReflectionMethod::IS_PROTECTED);
+
+        $blacklist = [
+            "__reflectPropertyes",
+            "__reflectMethods",
+            "__reflectDataFromSure",
+            "__toRender",
+            "__toString",
+            "__invoke",
+            "__construct"
+        ];
+
+        foreach ($methods as $key => $method) {
+
+            $name = $method->getName();
+            if (false === array_search($name, $blacklist)) {
+
+                $parameters = $method->getParameters();
+                $mdata = [
+                    "name" => $name,
+                    "params" => []
+                ];
+
+                foreach ($parameters  as $key => $parameter) {
+                    $mdata["params"][$parameter->getName()] = [
+                        "type" => $parameter->getType(),
+                        "index" => $parameter->getPosition(),
+                        "required" => !$parameter->isOptional(),
+                        "default" => $parameter->isOptional() ? $parameter->getDefaultValue() : null
+                    ];
+                }
+
+                $data[] = $mdata;
+            }
+        }
+
+        return $data;
+    }
+
+
+    public static function __toBuild(): Data
+    {
+        $reflection = new ReflectionClass(self::class);
+        $methods = self::__reflectDataFromSure($reflection);
+
+        $name = $reflection->getShortName();
+        $filename = $reflection->getFileName();
+        $namespace = $reflection->getNamespaceName();
+
+        self::$__template_name = join(".", [$name, self::$template_file_conf['ext']]);
+        self::$__template_name = Tools::path_join(DIRECTORY_SEPARATOR, dirname($filename), self::$__template_name);
+
+        if (!file_exists(self::$__template_name)) {
+            throw new Error("Not Found Template File $name from [$namespace] controler.", 500);
+        }
+
+        if (!is_readable(self::$__template_name)) {
+            throw new Error("Template File [$name] doesn't us readable", 500);
+        }
+
+        $data = new Data;
+        $data->name = self::$__template_name;
+        $data->helpers = $methods;
+        $data->namespace = $namespace;
+        return $data;
+    }
+
+    public function __toRender(): Data
+    {
+        $reflection = new ReflectionClass($this);
+
+        $buildData = self::__toBuild();
+
+        $data = new Data;
+        $data->name = $buildData->name;
+        $data->namespace = $buildData->namespace;
+        $data->properties = $this->__reflectPropertyes($reflection);
+        $data->helpers =  $this->__reflectMethods($reflection);
+
+        return $data;
     }
 }
