9daa8cce393a31475b8853c23c5d14d25b697287
Ezequiel Garcia <egarcia.informatic.ing@gmail.com>
2020-08-21 19:49:29 -0400

    added data constructor and future render function

---
+++ src/Template/Render/Engine.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Core\Template\Render;
+
+use Core\Template\TemplateInterface;
+use Core\Template\Template;
+use LightnCandy\LightnCandy;
+
+class Engine
+{
+    private $filesystem;
+
+    public function __construct()
+    {
+    }
+
+    public function helperLoader()
+    {
+    }
+
+    public function compilePartial(string $path)
+    {
+    }
+
+    public function compileComponent(Data $input)
+    {
+    }
+
+    public function render(Data $input)
+    {
+    }
+
+    public function build()
+    {
+    }
+}
