18afa1ea2f69750bd528965102cf0e95d32af4d3
Ezequiel Garcia <egarcia.informatic.ing@gmail.com>
2020-08-23 02:03:23 -0400

    added list of reserved names from  properties and class methods

---
+++ src/Template/Lib/reserved_names.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    // properties
+    "yield",
+    "Template",
+    "template_file_conf",
+    "__template_name",
+    // methods
+    "__isLayout",
+    "__isTemplate",
+    "setYield",
+    "__reflectPropertyes",
+    "__reflectMethods",
+    "__reflectDataFromSure",
+    "__toRender",
+    "__toBuild",
+    "__toString",
+    "__invoke",
+    "__construct"
+];
