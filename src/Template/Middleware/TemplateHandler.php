70c6fe99ed4038ad73c837bcf8cc8066a2252334
Ezequiel Garcia <egarcia.informatic.ing@gmail.com>
2020-08-26 17:41:40 -0400

    updated the TemplateHandler to integrate the new Rendering Engine

--- src/Template/Middleware/TemplateHandler.php
+++ src/Template/Middleware/TemplateHandler.php
@@ -3,18 +3,22 @@
 namespace Core\Template\Middleware;
 
 use Core\Application;
+use Core\Http\Router;
 use Core\Http\Message\RouterStatus;
 use Core\Http\Message\RouterStatusInterface;
-use Core\Template\Template;
+use Core\Template\LayoutInterface;
+use Core\Template\Render\Data;
 use Core\Template\TemplateInterface;
+use Core\Template\Render\Engine as TemplateEngine;
 
 use HttpStatusCodes\HttpStatusCodes as StatusCode;
-use LightnCandy\LightnCandy;
-
+use Laminas\Diactoros\Response\HtmlResponse;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+
+use function Core\Template\Lib\{is_template};
 
 class TemplateHandler implements MiddlewareInterface
 {
@@ -37,44 +41,50 @@ class TemplateHandler implements MiddlewareInterface
             return $handler->handle($request);
         }
 
+        $missing_response_error = StatusCode::getDescription(StatusCode::HTTP_EXPECTATION_FAILED_CODE);
+
         /** @var ResponseInterface $response */
         $response = ($this->responseFactory)();
-        $handler = $status->getHandler();
-        
-        if (is_callable($handler)) {
-            if (($res = $handler($request)) instanceof ResponseInterface) {
-                return $res;
-            }
 
-            $response->getBody()->write($res);
-            return $response;
+        /** @var callable|string|null $routeHandler */
+        $routeHandler = $status->getHandler();
+
+        if (!$routeHandler) {
+            $status = 
+            $request = $request->withAttribute(RouterStatusInterface::class, $status);
+            return $handler->handle($request);
         }
 
-        if (class_exists($handler)) {
-            $isTemplate = array_search(Template::class, class_uses($handler)) !== false;
-            if ($isTemplate) {
-                /** @var TemplateInterface $controller */
-                $controller = new $handler;
-                $result = $controller->__toRender();
-                $compiled = LightnCandy::compile(file_get_contents($result->name), [
-                    "flags" =>
-                    LightnCandy::FLAG_RENDER_DEBUG |
-                        LightnCandy::FLAG_BESTPERFORMANCE |
-                        LightnCandy::FLAG_HANDLEBARSJS |
-                        LightnCandy::FLAG_INSTANCE |
-                        LightnCandy::FLAG_ADVARNAME,
-                ]);
+        if (is_callable($routeHandler)) {
+            $result = call_user_func($routeHandler, $request);
+        } else if (class_exists($routeHandler)) {
 
-                $renderer = LightnCandy::prepare($compiled);
-                $html = $renderer($controller);
+            /** @var TemplateInterface $controller */
+            $controller = $this->app->get($routeHandler);
 
-                $response->getBody()->write($html);
+            if (is_template($routeHandler)) {
+                /** @var Router $router */
+                $router = $this->app->get(Router::class);
+
+                /** @var TemplateEngine $templating*/
+                $templating = $this->app->get(TemplateEngine::class);
+
+                /** @var LayoutInterface|null $controller */
+                $layout = $router->getLayout();
+
+                $html = $layout ? $templating->layout($layout, $controller) : $templating->render($controller);
+                $result = new HtmlResponse($html, 200);
             } else {
-                $response = $response->withStatus(500, "Dont use Template");
-                $response->getBody()->write("Dont use Template");
+
+                $result = is_callable($controller) ? $controller($request) : $missing_response_error;
             }
         }
 
+        if ($result instanceof ResponseInterface) {
+            return $result;
+        }
+
+        $response->getBody()->write($result);
         return $response;
     }
 }
