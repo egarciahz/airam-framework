2e4469aeecbd3138fcb1a38f383b984b766389fe
Ezequiel Garcia <egarcia.informatic.ing@gmail.com>
2020-08-28 11:53:18 -0400

    major changes in template functions
    - added ClosureCodeCompiler,passport functions for prepare the code of helpers.
    - updated matchFilesByExtension function from exclude directories support

--- src/Template/Lib/functions.php
+++ src/Template/Lib/functions.php
@@ -8,6 +8,7 @@ use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 use ReflectionClass;
 use RegexIterator;
+use Closure;
 
 use function Airam\Utils\closureFactory;
 use function Airam\Utils\path_join;
@@ -21,15 +22,22 @@ function makeTemplateFileName(string $origin)
     return $name;
 }
 
-function matchFilesByExtension(string $folder, array $extensions, array $fileList = [])
+function matchFilesByExtension(string $folder, array $extensions, array $ignoreDirs = [], ?array $fileList = [])
 {
     $dir = new RecursiveDirectoryIterator($folder);
     $iter = new RecursiveIteratorIterator($dir);
     $files = new RegexIterator($iter, '/(' . path_join("|", $extensions) . ')$/');
     $files->setMode(RegexIterator::MATCH);
 
+    $exclude =  "({$folder}" . DIRECTORY_SEPARATOR . "+)*(" . path_join("|", $ignoreDirs) . ")";
+    $exclude = "/^" . str_replace(DIRECTORY_SEPARATOR, "\\" . DIRECTORY_SEPARATOR, $exclude) . ".+$/";
+
     foreach ($files as $file) {
-        $fileList[] = $file->getPathname();
+        $path = $file->getPathname();
+        if (count($ignoreDirs) > 0 && preg_match($exclude, $path) !== 0) {
+            continue;
+        }
+        $fileList[] = $path;
     }
 
     return $fileList;
@@ -56,4 +64,21 @@ function is_template($ref)
 function is_layout($ref)
 {
     return class_use($ref, Template::class) && class_use($ref, Layout::class);
+}
+
+function closureCodeCompiler(Closure $closure, string $name)
+{
+    if ($closure instanceof Closure) {
+        $code = closureFactory($closure);
+        return " \"{$name}\" => {$code}";
+    }
+    return " \"{$name}\" => \"passport\" ";
+}
+
+/**
+ * neutral element of render
+ */
+function passport()
+{
+    return null;
 }
