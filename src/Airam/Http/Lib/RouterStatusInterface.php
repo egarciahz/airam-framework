64de165cb1b573154ddf305c7b0f1e743966f364
Ezequiel Garcia <egarcia.informatic.ing@gmail.com>
2020-08-30 00:42:19 -0400

    moved Http interfaces to folder Lib

--- src/Airam/Http/Lib/RouterStatusInterface.php
+++ src/Airam/Http/Lib/RouterStatusInterface.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace Airam\Http\Message;
+namespace Airam\Http\Lib;
+
+use Laminas\Uri\UriInterface;
 
 interface RouterStatusInterface
 {
@@ -10,4 +12,5 @@ interface RouterStatusInterface
     public function getHandler();
     public function getParams(): array;
     public function getStatus(): int;
+    public function getUri(): UriInterface;
 }
