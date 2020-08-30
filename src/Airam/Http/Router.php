99a525182f125d0bd6a0c00644ae5190ec9c55a0
Ezequiel Garcia <egarcia.informatic.ing@gmail.com>
2020-08-30 00:51:05 -0400

    added constants of passport middleware attribute values

--- src/Airam/Http/Router.php
+++ src/Airam/Http/Router.php
@@ -5,19 +5,15 @@ namespace Airam\Http;
 use FastRoute\{RouteCollector, Dispatcher};
 use FastRoute\Dispatcher\GroupCountBased;
 
-use Airam\Application;
 use Airam\Template\Render\Rendereable;
 
 class Router extends RouteCollector implements Dispatcher
 {
     use Rendereable;
 
-    /**
-     * @Inject
-     * @var Application $app
-     */
-    private $app;
-
+    const HANDLE_STATUS_CODE = "0x_RouterStatusData.Code$";
+    const HANDLE_MODULE_CODE = "1x_RouterModuleRef.Code$";
+    
     public function dispatch($httpMethod, $uri)
     {
         $base = new GroupCountBased($this->getData());
