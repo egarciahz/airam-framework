4ad42919ad32407f3179897c63b3ad656824d132
Ezequiel Garcia <egarcia.informatic.ing@gmail.com>
2020-08-30 00:43:09 -0400

    updated RouterStatus for handle a laminas UriInterface

--- src/Airam/Http/Message/RouterStatus.php
+++ src/Airam/Http/Message/RouterStatus.php
@@ -2,22 +2,28 @@
 
 namespace Airam\Http\Message;
 
+use Airam\Http\Lib\RouterStatusInterface;
+use Laminas\Uri\UriInterface;
+
 class RouterStatus implements RouterStatusInterface
 {
     private $handler;
     private $params;
     private $status;
+    private $uri;
 
     /**
      * @param int $status
      * @param array $params
      * @param string|array|callable $handler
+     * @param UriInterface $uri
      */
-    public function __construct(int $status, array $params = null, $handler = null)
+    public function __construct(int $status, UriInterface $uri, array $params = null, $handler = null)
     {
         $this->status = $status;
         $this->params = $params;
         $this->handler = $handler;
+        $this->uri = $uri;
     }
 
     public function getHandler()
@@ -33,5 +39,10 @@ class RouterStatus implements RouterStatusInterface
     public function getStatus(): int
     {
         return $this->status;
+    }
+
+    public function getUri(): UriInterface
+    {
+        return $this->uri;
     }
 }
