6cf62ec462b53d2ffb27fd37773986bffae29947
Ezequiel Garcia <egarcia.informatic.ing@gmail.com>
2020-09-01 01:10:49 -0400

    removed Rendereable trait dependency of Router

--- src/Airam/Http/Router.php
+++ src/Airam/Http/Router.php
@@ -5,12 +5,10 @@ namespace Airam\Http;
 use FastRoute\{RouteCollector, Dispatcher};
 use FastRoute\Dispatcher\GroupCountBased;
 
-use Airam\Template\Render\Rendereable;
+use function Airam\Commons\{loadResource, path_join};
 
 class Router extends RouteCollector implements Dispatcher
 {
-    use Rendereable;
-
     const HANDLE_STATUS_CODE = "0x_RouterStatusData.Code$";
     const HANDLE_MODULE_CODE = "1x_RouterModuleRef.Code$";
     
