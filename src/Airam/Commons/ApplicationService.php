02344cc556932cb7b1c6303ef68920e5a1d0bfe1
Ezequiel Garcia <egarcia.informatic.ing@gmail.com>
2020-09-03 18:14:25 -0400

    Changed namespace name from Common to Commons

---
+++ src/Airam/Commons/ApplicationService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Airam\Commons;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Message\ResponseInterface;
+
+interface ApplicationService
+{
+    public function __construct(ContainerInterface $app);
+
+    public function app(): ContainerInterface;
+
+    public function register(string $path): void;
+
+    public function pushMiddleware(MiddlewareInterface $middleware): void;
+
+    public function run(ServerRequestInterface $request): ResponseInterface;
+}
