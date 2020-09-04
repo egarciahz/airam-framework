77a8025ac0a30e0955a531def840ab3df026c495
Ezequiel Garcia <egarcia.informatic.ing@gmail.com>
2020-09-04 00:49:10 -0400

    starting a common code compiler
    this class is expected to compile code, package it, and make files

---
+++ src/Airam/Commons/Compiler/DataTokens.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Airam\Commons\Compiler;
+
+class DataTokens
+{
+    /** @var string $code */
+    public $code;
+    /** @var array<string> $usages */
+    public $usages;
+    /** @var string|null $namespaceName */
+    public $namespaceName;
+}
