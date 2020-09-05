7cd5ef9d0244ef0a8e237850e38344fc4b400cc4
Ezequiel Garcia <egarcia.informatic.ing@gmail.com>
2020-09-05 01:51:01 -0400

    moved matchFilesByExtension function from Template\Lib to Commons

--- src/Airam/Template/Lib/functions.php
+++ src/Airam/Template/Lib/functions.php
@@ -4,13 +4,9 @@ namespace Airam\Template\Lib;
 
 use Airam\Template\{Template, Layout};
 
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
-use RegexIterator;
 use Closure;
 
 use function Airam\Commons\{
-    path_join,
     class_use,
     closureFactory
 };
@@ -22,27 +18,6 @@ function makeTemplateFileName(string $origin)
     $name = str_replace(DIRECTORY_SEPARATOR, "_", $name);
     $name = preg_replace("/\..+$/", ".php", $name);
     return $name;
-}
-
-function matchFilesByExtension(string $folder, array $extensions, array $ignoreDirs = [], ?array $fileList = [])
-{
-    $dir = new RecursiveDirectoryIterator($folder);
-    $iter = new RecursiveIteratorIterator($dir);
-    $files = new RegexIterator($iter, '/(' . path_join("|", $extensions) . ')$/');
-    $files->setMode(RegexIterator::MATCH);
-
-    $exclude =  "({$folder}" . DIRECTORY_SEPARATOR . "+)*(" . path_join("|", $ignoreDirs) . ")";
-    $exclude = "/^" . str_replace(DIRECTORY_SEPARATOR, "\\" . DIRECTORY_SEPARATOR, $exclude) . ".+$/";
-
-    foreach ($files as $file) {
-        $path = $file->getPathname();
-        if (count($ignoreDirs) > 0 && preg_match($exclude, $path) !== 0) {
-            continue;
-        }
-        $fileList[] = $path;
-    }
-
-    return $fileList;
 }
 
 function is_template($ref)
