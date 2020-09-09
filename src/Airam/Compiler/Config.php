dee81505b36471c47f50c796eb43dc51b2f7decb
Ezequiel Garcia <egarcia.informatic.ing@gmail.com>
2020-09-09 02:02:58 -0400

    updated helper constructors for the new Compiler schema definitions

--- src/Airam/Compiler/Config.php
+++ src/Airam/Compiler/Config.php
@@ -1,10 +1,12 @@
 <?php
 
-namespace Airam\Commons\Compiler;
+namespace Airam\Compiler;
+
+use function Airam\Commons\path_join;
 
 class Config
 {
-    public $dirname;
+    public $root;
     public $watch;
     public $subdirs;
 
@@ -15,8 +17,17 @@ class Config
      */
     public function __construct(string $dirname, string $watch, array $subdirs = [])
     {
-        $this->dirname = $dirname;
+        $this->root = realpath(path_join(DIRECTORY_SEPARATOR, getenv("ROOT_DIR"), $dirname));
         $this->subdirs = $subdirs;
         $this->watch = $watch;
+    }
+
+    public static function fromArray(array $config): Config
+    {
+        return new Config($config["root"], $config["watch"], $config["subdirs"]);
+    }
+
+    public function build(){
+        FileSystem::makeDirectoryMap($this->root, $this->subdirs);
     }
 }
--- src/Airam/Compiler/DirMap.php
+++ src/Airam/Compiler/DirMap.php
@@ -1,22 +1,25 @@
 <?php
 
-namespace Airam\Commons\Compiler;
+namespace Airam\Compiler;
+
+use Dotenv\Store\FileStore;
+use RuntimeException;
 
 class DirMap
 {
-    /** @var Config $config*/
+    /** @var Config $config compiler configuration*/
     private $config;
 
-    /** @var string $target*/
+    /** @var string $target path to compiled file*/
     private $target;
 
-    /** @var string $watch*/
+    /** @var string $watch path to observable folder*/
     private $watch;
 
     /** @var array<string> $files list of available file extensions*/
     public $files = [];
 
-    /** @var array<string> $exclude exclude directories under dirname path*/
+    /** @var array<string> $exclude exclude directories under watchable path*/
     public $exclude = [];
 
     /**
@@ -25,44 +28,70 @@ class DirMap
      * @var array<string> $files
      * @var array<string> $exclude
      */
-    public function __construct(Config $config, string $target, string $watch, array $files, array $exclude = [])
+    public function __construct(Config $config, string $target, ?array $files, ?string $watch, array $exclude = [])
     {
+        $this->config = $config;
+
+        $this->watch =  $this->prepare($watch);
         $this->target = $target;
-        $this->watch = $watch;
         $this->files = $files;
         $this->exclude = $exclude;
-        $this->config = $config;
     }
 
-    public function getTarget(string $filename = "")
+    public static function fromSchema(array $schema, string $target): DirMap
     {
-        $target = $this->prepare($this->target);
-        $target = str_replace("{filename}", $filename, $this->target);
-        return $target;
+        $config = Config::fromArray($schema["config"]);
+        $map = $schema[$target];
+        $watch = $map["watch"];
+
+        if ($watch) {
+            return new DirMap($config, $map["target"], $watch["files"], $watch["dirname"], $watch["exclude"]);
+        }
+
+        return new DirMap($config, $map["target"], null, null);
     }
 
-    public function getDirname()
+    public function getPath(string $name): string
     {
-        $dirname = $this->prepare($this->watch);
-        return $dirname;
+        return $this->prepare($this->target, ["filename" => $name]);
     }
 
-    private function prepare(string $path)
+    public function isFileExist(string $name): bool
     {
-        if (preg_match_all("/\{([a-z])\}/", $path, $matches)) {
+        return file_exists($this->getPath($name));
+    }
+
+    public function getDirname(): string
+    {
+        return $this->watch;
+    }
+
+    private function prepare(?string $path, array $scope = []): ?string
+    {
+        if (!$path) {
+            return null;
+        }
+
+        $matches = [];
+        if (preg_match_all("/\{*([a-z]+)\}/", $path, $matches)) {
             if ($matches) {
                 foreach ($matches[1] as $tag) {
-                    $value = isset($this->config->{$tag}) ? $this->config->{$tag} : null;
-                    if (!$value) {
-                        error_log("unrecognized ($tag) property of compiler.config");
-                        continue;
+                    $value = isset($this->config->{$tag}) ? $this->config->{$tag} : ($scope[$tag] ?: false);
+                    if ($value === false) {
+                        throw new RuntimeException(sprintf("Unrecognized (%s) property of compiler['config'] ", $tag));
                     }
 
-                    $path = str_replace("\{{$tag}\}", $value, $path);
+                    $path = str_replace("{{$tag}}", $value, $path);
                 }
             }
         }
 
         return $path;
+    }
+
+    public function build()
+    {
+        $target = $this->prepare(dirname($this->target));
+        FileSystem::makeDirectory($target);
     }
 }
